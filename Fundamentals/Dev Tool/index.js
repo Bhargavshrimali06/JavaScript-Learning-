d8e49efe85fa59a648acea7f28c0a79cf07f75bf
Bhargav shrimali <bhargavshrimali3906@gmail.com>
2026-07-28 22:46:03 +0530

    testing dev tools

--- Fundamentals/Dev Tool/index.js
+++ Fundamentals/Dev Tool/index.js
@@ -2,14 +2,20 @@ const userName = document.getElementById("userName");
 const passWord = document.getElementById("passWord");
 const message = document.getElementById("loginMsg");
 
+//this loop is made to use breakpoints set one on console log and 
+// reload the page u will be able to see each number it goes trough.
+ for (let i = 0; i <= 5; i++) {
+    console.log(i);
+ }
 
+ 
 function Login() {
     let name = userName.value;
     let password = passWord.value;
 
     // debugger;
 
-    if (name == "Bhargav" && password == "Bhargav@1") {
+    if (name === "Bhargav" && password === "Bhargav@1") {
         message.textContent = "login Successful!";
     } else {
         message.textContent = "Failed try again!!";
@@ -18,3 +24,4 @@ function Login() {
 
 
 }
+
