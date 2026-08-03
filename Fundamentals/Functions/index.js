f2cba5d406ca2dacc518ae7dbe81e7811ce533d3
Bhargav shrimali <bhargavshrimali3906@gmail.com>
2026-08-03 16:50:26 +0530

    testing more features of functions

--- Fundamentals/Functions/index.js
+++ Fundamentals/Functions/index.js
@@ -1,49 +1,63 @@
-
-
-
-//A simple Function to test 
-function favoriteAnimal(animal) 
-{
-    return animal + "is my favorite Animal!"
+//A simple Function to test
+function favoriteAnimal(animal) {
+  return animal + "is my favorite Animal!";
 }
 
-const message = favoriteAnimal('horsh')
-console.log(favoriteAnimal('turtal'))
+const message = favoriteAnimal("horse");
+console.log(favoriteAnimal("turtle"));
 
-
-function greet()
-{
-    alert("Hello, Welcome User!!")
+function greet() {
+  alert("Hello, Welcome User!!");
 }
 
-greet()
+greet();
 
-//testing if function can chnage value of outer variable 
+//testing if function can chnage value of outer variable
 let userName = "joy";
 
-function changeName()
-{
-   let userName = "Bob";
-    let message = "hello " + userName;
-    alert(message);
+function changeName() {
+  let userName = "Bob";
+  let message = "hello " + userName;
+  alert(message);
 }
 
-alert(userName);
+// alert(userName);
 
 changeName();
 
 // alert(userName);
 
-function call(parameter1, paramater2 = "no text given!!") //if no text is given as argument for parameter2 it will show this text instead
-{
-   parameter1 = "'" + parameter1 + "'";
+function call(parameter1, parameter2 = "no text given!!") {
+  //if no text is given as argument for parameter2 it will show this text instead
+  parameter1 = "'" + parameter1 + "'";
 
-   alert( parameter1 + ": " + paramater2);
-
+  alert(parameter1 + ": " + parameter2);
 }
- 
+
 let parameter1 = "Joy";
 
-call(parameter1, "Konichiwa!!");
+call(parameter1, "Konnichiwa!!");
 
-alert(parameter1);
+// alert(parameter1);
+
+let myName = null; //testing nullish coalescing operator
+
+console.log(myName ?? "guest"); //if name is null or undefined it will choose the guest
+
+//testing return
+function checkAge(age) {
+  if (age >= 18) {
+    return true;
+  } else {
+    return confirm("Get permission from an Adult:");
+  }
+}
+
+let age = prompt("How old are you?", 19);
+
+
+if (checkAge(age)) {
+  alert("Access granted");
+} else {
+  alert("Access denied");
+}
