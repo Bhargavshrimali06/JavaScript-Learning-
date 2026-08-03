1db11ce15604d5529a849a8997cff96430a07c8a
Bhargav shrimali <bhargavshrimali3906@gmail.com>
2026-08-03 15:09:37 +0530

    testing default parameters

--- Fundamentals/Functions/index.js
+++ Fundamentals/Functions/index.js
@@ -33,3 +33,17 @@ alert(userName);
 changeName();
 
 // alert(userName);
+
+function call(parameter1, paramater2 = "no text given!!") //if no text is given as argument for parameter2 it will show this text instead
+{
+   parameter1 = "'" + parameter1 + "'";
+
+   alert( parameter1 + ": " + paramater2);
+
+}
+ 
+let parameter1 = "Joy";
+
+call(parameter1, "Konichiwa!!");
+
+alert(parameter1);
