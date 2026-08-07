b2996d0efb8ce60f7a20b9e757a969707df3d9f6
Bhargav shrimali <bhargavshrimali3906@gmail.com>
2026-08-07 16:08:41 +0530

    Inbuild functions again

--- Fundamentals/Functions/function-part2/index.js
+++ Fundamentals/Functions/function-part2/index.js
@@ -1,5 +1,12 @@
 //trying Inbuild Functions 
 const myText = "Hello!! I am John";
-const myNewText = myText.replace("John", "Bhargav");
+const myNewText = myText.replace("John", "Bhargav");//replace does what it says 
 
 console.log(myNewText);
+
+const randomArray = ["I","am","Iron", "Men" ];
+const madeAString = randomArray.join(" ");
+console.log(madeAString); //So Join joins all the array together 
+
+const myNum = Math.random();
+console.log(myNum);
