eae5c14b366a32b5ee995ce5cc3a2f00823a2fef
Bhargav shrimali <bhargavshrimali3906@gmail.com>
2026-08-12 15:46:40 +0530

    anonymous and arrow function

--- Fundamentals/Functions/function-part2/index.js
+++ Fundamentals/Functions/function-part2/index.js
@@ -4,9 +4,33 @@ const myNewText = myText.replace("John", "Bhargav");//replace does what it says
 
 console.log(myNewText);
 
-const randomArray = ["I","am","Iron", "Men" ];
+const randomArray = ["I","am","Iron", "Man" ];
 const madeAString = randomArray.join(" ");
 console.log(madeAString); //So Join joins all the array together 
 
 const myNum = Math.random();
-console.log(myNum);
+console.log(myNum);//Random is just random 
+
+//Invoking function
+function myFunction() {
+    alert("hellllooooooo");
+
+}
+
+myFunction();
+//Anonymous Function 
+const greet = function()
+{
+    console.log("this is an anonymous function!!");
+
+}
+setTimeout(greet, 5000);//setTimeout set time like 5000 is 5 sec so greet will lauch after 5sec ezzz
+
+
+const arrays = [1,2,3];
+
+console.log(arrays);
+
+const double = arrays.map(item => item *2);
+
+console.log(double);
