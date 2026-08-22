7b44a5bea89101b36cf4a8caea9ffe6ce1dd5a4b
Bhargav shrimali <bhargavshrimali3906@gmail.com>
2026-08-22 16:37:24 +0530

    Build some random function to get used to them

--- Fundamentals/Functions/function-part3/index.js
+++ Fundamentals/Functions/function-part3/index.js
@@ -34,3 +34,26 @@ ask("Do you Agree with our Terms & Condition?"
     ,function() { alert("You have agreed.")}  //this is short method very easy to write and read
     ,function() { alert("You have disagreed.")}
 );
+
+// more info in function declaration u can access them before function line appears,
+// and in function expression u can not do same. 
+
+const number1 = document.getElementById("num1");
+const number2 = document.getElementById("num2");
+
+function addNum(a, b)
+{
+    return a + b;
+}
+
+function showResult() {
+    const result = addNum (
+        Number(number1.value),
+        Number(number2.value)
+    )
+
+    const showAns = document.createElement("p");
+    showAns.textContent = result;
+
+    document.body.appendChild(showAns);
+}
